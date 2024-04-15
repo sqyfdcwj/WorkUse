4f650361c495d213d4630ec834a63dfcd6137bce
sqyfdcwj <cmlau1998@gmail.com>
2024-04-15 17:24:48 +0800

    Update DBConn.php

--- WebApi/Lib/DBConn.php
+++ WebApi/Lib/DBConn.php
@@ -7,38 +7,55 @@ require_once 'SQLUtil.php';
  */
 final class DBConn
 {
-    private array $dbInfo = [];
-
     /**
-     * @return array Database info, available keys:
+     * @var array $dbInfo Database info, available keys:
      * host: Database host
      * port: Database port
      * dbname: Database name
      * dsn: DataSource name which can be used in PDO::__construct
      * dsn2: DataSource name which is more readable, does not contain driver name
      */
+    private array $dbInfo = [];
     public function getDBInfo(): array { return $this->dbInfo; }
     
+    /**
+     * The internal PDO object which provides database functionality
+     * @var PDO $pdo
+     */
     private PDO $pdo;
 
+    /**
+     * @param string $driver Database driver
+     * @param string $host Database host
+     * @param int $port Database port
+     * @param string $dbname Database name
+     * @param string $user Database user
+     * @param string $pwd Database password
+     * @throws PDOException When failed to connect database
+     */
     public function __construct(
         string $driver, string $host, int $port, string $dbname, 
         string $user, string $pwd
     ) {
         $dsn = self::getDSN($driver, $host, $port, $dbname);
-        $this->dbInfo["driver"] = $driver;
-        $this->dbInfo["host"] = $host;
-        $this->dbInfo["port"] = $port;
-        $this->dbInfo["dbname"] = $dbname;
-        $this->dbInfo["dsn"] = $dsn;
-        $this->dbInfo["dsn2"] = "$host:$port $dbname";
+        $this->dbInfo = [
+            "driver" => $driver, "host" => $host, "port" => $port, 
+            "dbname" => $dbname, "dsn" => $dsn, "dsn2" => "$host:$port $dbname"
+        ];
         $options = [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
         ];
-        // Potential PDOException, should be handled properly.
-        $this->pdo = new PDO($dsn, $user, $pwd, $options);
+        $this->pdo = new PDO($dsn, $user, $pwd, $options);  // Potential PDOException
     }
 
+    /**
+     * Get a datasource name string which can be used as 1st param of PDO::__construct
+     * @param string $driver Database driver, supported values: pgsql(PostgreSQL), mssql(Microsoft Server)
+     * @param string $host Database host
+     * @param int $port Database port
+     * @param string $dbname Database name
+     * @return string
+     */
     public static function getDSN(string $driver, string $host, int $port, string $dbname): string 
     {
         if ($driver === "pgsql") {
@@ -49,16 +66,24 @@ final class DBConn
             return "";
         }
     }
+
+    /**
+     * Get a PostgreSQL connection
+     * @return self
+     */
     public static function pg(string $host, int $port, string $dbname, string $user, string $pwd): self 
     { return new DBConn("pgsql", $host, $port, $dbname, $user, $pwd); }
 
+    /**
+     * Get a Microsoft Server connection
+     */
     public static function mssql(string $host, int $port, string $dbname, string $user, string $pwd): self
     { return new DBConn("sqlsrv", $host, $port, $dbname, $user, $pwd); }
 
     public function inTransaction(): bool { return $this->pdo->inTransaction(); }
 
     /**
-     * @throws PDOException Active transaction
+     * @throws PDOException If the connection has already an active transaction
      * @return OpResult
      */
     public function beginTransaction(bool $isThrowEx = FALSE): OpResult 
@@ -67,7 +92,7 @@ final class DBConn
     }
 
     /**
-     * @throws PDOException No active transaction
+     * @throws PDOException If the connection has no active transaction
      * @return OpResult
      */
     public function commit(bool $isThrowEx = FALSE): OpResult 
@@ -76,7 +101,7 @@ final class DBConn
     }
 
     /**
-     * @throws PDOException No active transaction
+     * @throws PDOException If the connection has no active transaction
      * @return OpResult
      */
     public function rollBack(bool $isThrowEx = FALSE): OpResult 
@@ -113,7 +138,7 @@ final class DBConn
         $caughtEx = NULL;
         try {
             if ($opContext->getIsTcl()) {
-                // If TCL operation raised exception, sqlState will be NULL
+                // sqlState will be NULL if PDOException is raised by a TCL operation
                 switch ($opContext->getSql()) {
                     case "beginTransaction":
                         $this->pdo->beginTransaction();
@@ -141,13 +166,7 @@ final class DBConn
             }
         }
 
-        return new OpResult(
-            $opContext,
-            $caughtEx,
-            $sqlState,
-            $rowCount,
-            $dataSet
-        );
+        return new OpResult($opContext, $caughtEx, $sqlState, $rowCount, $dataSet);
     }
 }
 
@@ -199,6 +218,12 @@ final class OpContext
     public function setTag(string $key, string $tag): void { $this->tags[$key] = $tag; }
     public function getTag(string $key): string { return $this->tags[$key] ?? ""; }
 
+    /**
+     * @param string $sql SQL query string
+     * @param array $rawParam SQL statement parameter
+     * @param bool $isTcl Whether the statement is a transaction control language (begin / commit / rollBack)
+     * @param array $tags Custom string key-value paired array storing external info for reference
+     */
     private function __construct(string $sql, array $rawParam, bool $isTcl, array $tags = []) 
     {
         $this->sql = $sql;
@@ -208,10 +233,28 @@ final class OpContext
         $this->tags = $tags;
     }
 
-    public static function beginTransaction(): self { return new OpContext(__FUNCTION__, [], TRUE); }
-    public static function commit(): self { return new OpContext(__FUNCTION__, [], TRUE); }
-    public static function rollBack(): self { return new OpContext(__FUNCTION__, [], TRUE); }
+    /**
+     * Get a beginTransaction tcl command
+     * @return self
+     */
+    public static function beginTransaction(array $tags = []): self { return new OpContext(__FUNCTION__, [], TRUE, $tags); }
+    
+    /**
+     * Get a commit tcl command
+     * @return self
+     */
+    public static function commit(array $tags = []): self { return new OpContext(__FUNCTION__, [], TRUE, $tags); }
+    
+    /**
+     * Get a rollBack tcl command
+     * @return self
+     */
+    public static function rollBack(array $tags = []): self { return new OpContext(__FUNCTION__, [], TRUE, $tags); }
 
+    /**
+     * Create a non-tcl command
+     * @return self
+     */
     public static function nonTcl(string $sql, array $rawParam, array $tags = []): self 
     { return new OpContext($sql, $rawParam, FALSE, $tags); }
 }
@@ -224,14 +267,13 @@ final class OpResult
     public function getContext(): OpContext { return $this->ctxt; }
 
     /**
-     * @var string $sqlState 5-Digit String retrievied from
-     * PDO::errorInfo or PDOStatement::errorInfo or PDOException::errorInfo
+     * @var string $sqlState 5-Digit String retrievied from PDO::errorInfo / PDOStatement::errorInfo / PDOException::errorInfo
      */
     private string $sqlState = "";
     public function getSqlState(): string { return $this->sqlState; }
 
     /**
-     * @var ?Exception $ex The PDOException instance, null if the database operation is successful.
+     * @var ?Exception $ex Caught exception. In most case this is a PDOException instance
      */
     private ?Exception $ex = NULL;
     public function getException(): ?Exception { return $this->ex; }
@@ -241,40 +283,33 @@ final class OpResult
     /**
      * @return string Exception message if not null, otherwise empty string
      */
-    public function getErrMsg(): string 
-    { 
-        return $this->ex === NULL ? "" : $this->ex->getMessage(); 
-    }
+    public function getErrMsg(): string { return $this->ex === NULL ? "" : $this->ex->getMessage(); }
 
     /**
      * @return string Exception trace if not null, otherwise empty string
      */
-    public function getTrace(): string 
-    { 
-        return $this->ex === NULL ? "" : $this->ex->getTraceAsString(); 
-    }
+    public function getTrace(): string { return $this->ex === NULL ? "" : $this->ex->getTraceAsString(); }
 
     /**
-     * @var int $rowCount Number of rows affected by INSERT / UPDATE / DELETE statement,
-     * or the number of rows returned by SELECT statement
+     * @var int $rowCount Number of rows returned from SELECT statement, or affected by INSERT / UPDATE / DELETE statement
      */
     private int $rowCount = 0;
     public function getRowCount(): int { return $this->rowCount; }
 
     /**
-     * @var array $dataSet Rows returned by SELECT statement,
-     * or INSERT / UPDATE / DELETE statement with keyword 'RETURNING'
+     * @var array $dataSet Rows returned by SELECT statement, or INSERT / UPDATE / DELETE statement with keyword 'RETURNING'
      */
     private array $dataSet = [];
     public function getDataSet(): array { return $this->dataSet; }
 
-    public function __construct(
-        OpContext $ctxt,
-        ?Exception $ex,
-        string $sqlState, 
-        int $rowCount, 
-        array $dataSet
-    ) {
+    /**
+     * @param OpContext
+     * @param ?Exception $ex Caught exception
+     * @param string $sqlState
+     * @param int $rowCount Number of rows returned from SELECT statement, or affected by INSERT / UPDATE / DELETE statement
+     * @param array $dataSet Rows returned from a SELECT statement or INSERT / UPDATE / DELETE statement with keyword 'RETURNING'
+     */
+    public function __construct(OpContext $ctxt, ?Exception $ex, string $sqlState, int $rowCount, array $dataSet) {
         $this->ctxt = $ctxt;
         $this->ex = $ex;
         $this->sqlState = $sqlState;
