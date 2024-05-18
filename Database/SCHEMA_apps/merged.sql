1a5bd33e9bc825bd1caf6cb0ccda272c963a6671
sqyfdcwj <cmlau1998@gmail.com>
2024-05-18 10:42:45 +0800

    Update

--- Database/SCHEMA_apps/merged.sql
+++ Database/SCHEMA_apps/merged.sql
@@ -1,7 +1,15 @@
-create table apps.sys_api_sql_log (
-    sql_log_id serial primary key
-);
+create schema apps;
 
+create table apps.sys_api_sql_group_dtl ( sql_group_dtl_id serial primary key );
+alter table apps.sys_api_sql_group_dtl add column sql_group_name varchar(100);
+alter table apps.sys_api_sql_group_dtl add column sql_group_version integer;
+alter table apps.sys_api_sql_group_dtl add column sql_name varchar(200);
+alter table apps.sys_api_sql_group_dtl add column sql text;
+alter table apps.sys_api_sql_group_dtl add column sql_display_name varchar(200);
+alter table apps.sys_api_sql_group_dtl add column sql_order integer;
+alter table apps.sys_api_sql_group_dtl add column key_field varchar(100);
+
+create table apps.sys_api_sql_log ( sql_log_id serial primary key );
 alter table apps.sys_api_sql_log add column created_on timestamp(0) without time zone; 
 alter table apps.sys_api_sql_log alter column created_on set default now();
 alter table apps.sys_api_sql_log add column sql_group_name varchar(100);
