1bd3e1a839c222e97c20d5db35381f5750becf03
sqyfdcwj <cmlau1998@gmail.com>
2024-06-18 18:26:19 +0800

    Update

--- Study/CLang/main.c
+++ Study/CLang/main.c
@@ -6,12 +6,5 @@
 
 int main()
 {
-    FILE *f = fopen("a.txt", "r");
-    fpos_t pos;
-    flag(f);
-    printf("\n");
-    fseek(f, 2, SEEK_SET);
-    flag(f);
-    fsetpos(f, &pos);
     return 0;
 }
